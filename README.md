d90e217b3442f7152c2d8922e79c78cc14e63476
rm86626 <106156564+rm86626@users.noreply.github.com>
2022-06-11 15:50:02 +0800

    new alter

--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 npm install myproject142
 npm install myproject142 --save
 
-###功能：显示对象信息
 
 ### npmjs.com
 [项目版本地址](https://www.npmjs.com/package/myproject142)
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function showInfo(){
-    console.log("showInfo功能执行了，哇咔咔 new更新");
+    console.log("showInfo功能执行了，哇咔咔 new更新11");
 }
 //es6到出方式
 export default showInfo;
